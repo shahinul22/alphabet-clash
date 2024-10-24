82ef3374dfe08bc1e829edd6e10ca9d2b01cc133
shahinul007 <shahinul.alam007@gmail.com>
2024-10-24 15:54:17 +0600

    clash1

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,43 @@
+
+function getRandomAlphabet(){
+    const alphaberStrings = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphaberStrings.split('');
+
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+
+    // console.log(index, alphabet);
+    return alphabet;
+
+}
+function hideElementById(elementID){
+    const homeScreen = document.getElementById(elementID);
+    homeScreen.classList.add('hidden');
+}
+function showElementById(elementID){
+    const playGround = document.getElementById(elementID);
+    playGround.classList.remove('hidden');
+}
+function setBackgroundColor(elementID){
+    const alphabet = document.getElementById(elementID);
+    alphabet.classList.add('bg-purple-500')
+}
+function continuePlay(){
+    const alphabet = getRandomAlphabet();
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText=alphabet;
+    setBackgroundColor(alphabet);
+
+
+}
+
 function play(){
-    console.log('play now');
+    hideElementById('home-section')
+    showElementById('play-ground')
+
+    continuePlay()
 }
